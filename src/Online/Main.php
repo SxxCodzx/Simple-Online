c7ba04f98daa9bc095e2224c3ad625f0dcb04d2c
tbralex7@gmail.com <tbralex7@gmail.com>
2022-05-02 22:21:36 -0500

    Update Main.php

--- src/Online/Main.php
+++ src/Online/Main.php
@@ -1,2 +1,32 @@
 <?php
 
+namespace Online;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginManager as P;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener as L;
+use pocketmine\command\Command;
+
+
+class Main extends P implements L {
+  
+  public function onEnable(): void{
+    $this->getServer()->getLogger()->info("Simple Online Enable");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+  }
+  
+  public function onCommand(CommandSender $pl, Command $cmd, string $label, array $args): bool{
+    switch($cmd->getName()){
+      case "online":
+        if($pl->hasPermission("online.cmd")){
+          $online = count(Server::getInstance()->getOnlinePlayers());
+          $pl->sendMessage("§l§9Online §r§7» Jugadores En Linea §a$online");
+        }else{
+          $pl->sendMessage("No Perm");
+        }
+      break;
+    }
+    return true;
+  }
+}
